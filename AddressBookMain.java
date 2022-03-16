019449a108f23d2bd3c10cd4698211353f30a90d
Your Name <you@example.com>
2022-03-16 18:27:50 +0530

    Duplicate Contacts

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -11,32 +11,34 @@ public class AddressBookMain
 	 * create method choose() to select any of the option
 	 */
 	public void choose() {
-		MultipleAddressBook obj1 = new MultipleAddressBook();
-		AddressBook obj2 = new AddressBook();
+		MultipleAddressBook obj = new MultipleAddressBook();
 		while (true) {
-			System.out.println(
-					"Enter \n 1. To add The new AddressBook\n 2. To do AddressBook functions\n 3. To delete the AddressBook\n "
-							+ "4. To Print the AddressBook\n 5. To Print the contacts in AddressBook\n 6. To show search options\n 0. to exit");
+			System.out.println("Enter \n 1. To add The new AddressBook\n 2. To add contact in AddressBook\n "
+					+ "3. To edit the contact in AddressBook\n 4. To delete the contact in AddressBook\n 5. To delete the AddressBook\n "
+					+ "6. To Print the AddressBook\n 7. To Print the contacts in AddressBook\n 0. to exit");
 			Scanner scanner = new Scanner(System.in);
 			int choice = scanner.nextInt();
 			switch (choice) {
 			case 1:
-				obj1.addAddressBook();
+				obj.addAddressBook();
 				break;
 			case 2:
-				obj1.addressBookFunctions();
+				obj.addContact();
 				break;
 			case 3:
-				obj1.deleteBook();
+				obj.editContactInBook();
 				break;
 			case 4:
-				obj1.printBook();
+				obj.deleteContactInBook();
 				break;
 			case 5:
-				obj1.printContactsInBook();
+				obj.deleteAddressBook();
 				break;
 			case 6:
-				obj2.viewByOptions();
+				obj.printBook();
+				break;
+			case 7:
+				obj.printContactsInBook();
 				break;
 			case 0:
 				System.exit(0);
@@ -54,6 +56,9 @@ public class AddressBookMain
 	 */
 	public static void main(String[] args) {
 		AddressBookMain addressBookMain = new AddressBookMain();
+		/**
+		 * calling choose method
+		 */
 		addressBookMain.choose();
 	}
 }
